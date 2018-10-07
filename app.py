fb3f866eb113fb54453d3df418f1574e3fa611c0
Antero Duarte <a.fduarte1@gmail.com>
2018-10-07 22:11:43 +0100

    Initial page for each thing, main page shows hero, streaming search functionality

--- app.py
+++ app.py
@@ -1,12 +1,45 @@
-from flask import Flask, request
-from model import ARTISTS
+import random
+from flask import Flask, request, render_template, Response
+from model import ARTISTS, RELEASES, ARTIST_TO_RELEASES, get_releases, search_results_for
 
 app = Flask(__name__)
+app.template_folder = 'templates'
 
 
 @app.route("/", methods=['GET'])
 def index():
-    return "Hello world"
+    # Get 5 random artists
+    spotlight = []
+    for a in random.sample(list(ARTISTS), 5):
+        artist = ARTISTS[a]
+        releases = get_releases(artist)
+        obj = {
+            "artist": artist,
+            "release": RELEASES[releases.pop()] if releases else None
+        }
+        spotlight.append(obj)
+    return render_template('index.html', hero=spotlight)
+
+
+@app.route("/artists/<string:id>", methods=['GET'])
+def artists(id):
+    return "Artist id is: " + id
+
+
+@app.route("/releases/<string:id>", methods=['GET'])
+def releases(id):
+    return "Release id is: " + id
+
+
+@app.route("/tracks/<string:id>", methods=['GET'])
+def tracks(id):
+    return "Track id is: " + id
+
+
+@app.route("/search")
+def search():
+    query = request.args.get("query", None)
+    return Response(search_results_for(query), mimetype='text/html')
 
 
 if __name__ == " __main__ ":
--- model.py
+++ model.py
@@ -1,8 +1,19 @@
 from settings import DATA_DIR
+from flask import url_for
 import csv
+from difflib import SequenceMatcher
 
 ARTISTS = {}
 RELEASES = {}
+TRACKS = {}
+
+# Indices, for faster lookups
+
+# Index artists to their releases
+ARTIST_TO_RELEASES = {}
+ARTIST_TO_TRACKS = {}
+TEXTINDEX = {}
+
 with open(DATA_DIR + "/artists.csv") as file:
     reader = csv.reader(file, delimiter=',', quotechar='"')
     # skip headers
@@ -12,15 +23,21 @@ with open(DATA_DIR + "/artists.csv") as file:
         # csv headers: ["artist","name","country","lifespan"]
         # All data was validated on creation, so there's no need to validate here
         start, end = row[3].split(" ")
-        ARTISTS[row[0]] = dict(
+        a = dict(
             id=row[0],
             name=row[1],
             country=row[2],
             # Some bands don't have an end date. (still active)
             # Since the data was created with javascript, those fields are undefined
             # So we just switch them for python's none
-            lifespan=(start, end if end != "untitled" else None)
+            lifespan=(start, (end if end != "undefined" else None))
         )
+        ARTISTS[a['id']] = a
+        TEXTINDEX[a['name']] = {
+            "id": a['id'],
+            "type": "artists",
+            "name": a['name']
+        }
 
 with open(DATA_DIR + "/releases.csv") as file:
     reader = csv.reader(file, delimiter=',', quotechar='"')
@@ -36,11 +53,77 @@ with open(DATA_DIR + "/releases.csv") as file:
         # Even though a release on musicbrainz is called a release group and is associated with multiple
         # releases, for this use case, we don't care about that,
         # so we index the release by "mainrel" rather than "relgroup"
-        RELEASES[row[4]] = dict(
-            id=row[2],
+        r = dict(
+            id=row[4],
             relgroup=row[1],
             artist=row[0],
             name=row[2],
             reldate=row[3],
             cover=(row[5], row[6])
         )
+        RELEASES[r["id"]] = r
+        # Indicies
+        # Text index
+        TEXTINDEX[r['name']] = {
+            "id": r['id'],
+            "type": "releases",
+            "name": r['name']
+        }
+        # Index to artist
+        if r["artist"] in ARTIST_TO_RELEASES:
+            ARTIST_TO_RELEASES[r["artist"]].append(r["id"])
+        else:
+            ARTIST_TO_RELEASES[r["artist"]] = [r["id"]]
+
+with open(DATA_DIR + "/tracks.csv") as file:
+    reader = csv.reader(file, delimiter=',', quotechar='"')
+    # skip headers
+    next(reader)
+    for row in reader:
+        # Add tracks to the dict, using their musicbrainz id as the key.
+        # csv headers: ["release","format","track","name","position","duration"]
+        # All data was validated on creation, so there's no need to validate here.
+        # We store the id of a tracks's release, rather than a pointer to it in RELEASES
+        # to simulate a case where looking it up would be expensive (e.g: database)
+
+        # We index using track
+        t = dict(
+            id=row[2],
+            release=row[0],
+            format=row[1],
+            name=row[3],
+            position=row[4],
+            duration=row[5]
+        )
+        t["artist"] = RELEASES[t["release"]]["artist"]
+        TRACKS[t["id"]] = t
+        # Indices
+        # Text index
+        TEXTINDEX[t['name']] = {
+            "id": t['id'],
+            "type": "tracks",
+            "name": t['name']
+        }
+        # Index to artist
+        if t["artist"] in ARTIST_TO_TRACKS:
+            ARTIST_TO_TRACKS[t["artist"]].append(t["id"])
+        else:
+            ARTIST_TO_TRACKS[t["artist"]] = [t["id"]]
+
+
+def get_releases(artist):
+    # add 1 random release for each artist
+    # if the artist has indexed releases
+    if artist['id'] in ARTIST_TO_RELEASES:
+        return ARTIST_TO_RELEASES[artist['id']]
+    else:
+        return []
+
+
+def search_results_for(query):
+    for (k, v) in TEXTINDEX.items():
+        match = SequenceMatcher(None, k, query)
+        print(query, match.ratio(), k)
+        if match.ratio() > 0.6:
+            yield "<a data-score=\"" + str(match.ratio()) + "\" href=\"/" + v['type'] + "/" + v['id'] + "\">" + v[
+                'name'] + "</a>"
