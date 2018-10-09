dd51ba18f2b271c713b303372f949df30711c099
Antero Duarte <a.fduarte1@gmail.com>
2018-10-09 23:11:38 +0100

    Improvements in artist browse page and single artist page. Added sorting of releases by release date

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+import datetime
+import operator
 import random
 from flask import Flask, request, render_template, Response
 from model import ARTISTS, RELEASES, ARTIST_TO_RELEASES, get_releases, search_results_for
@@ -12,10 +14,10 @@ def index():
     spotlight = []
     for a in random.sample(list(ARTISTS), 5):
         artist = ARTISTS[a]
-        releases = get_releases(artist)
+        releases = sorted([RELEASES[x] for x in get_releases(artist)], key=operator.itemgetter('reldate'))
         obj = {
             "artist": artist,
-            "release": RELEASES[releases.pop()] if releases else None
+            "release": releases[0] if releases else None
         }
         spotlight.append(obj)
     return render_template('index.html', hero=spotlight)
@@ -24,12 +26,22 @@ def index():
 @app.route("/artists", methods=['GET'])
 @app.route("/artists/<string:id>", methods=['GET'])
 def artists(id=None):
+    # If there is no ID, render the artist browsing page
     if id is None:
         letter = request.args.get("l", "a")
-        artists = [a for (k, a) in ARTISTS.items() if a['name'].lower().startswith(letter)]
-        return render_template("artists.html", artists=artists)
+        artists = [a for (k, a) in ARTISTS.items() if a['name'].lower().startswith(letter.lower())]
+        artists.sort(key=operator.itemgetter('name'))
+        artist_tuples = []
+        for a in artists:
+            releases = sorted([RELEASES[x] for x in get_releases(a)], key=operator.itemgetter('reldate'))
+            artist_tuples.append((a, releases[0] if releases else None))
+        return render_template("artists.html", artists=artist_tuples, alphabet=map(chr, range(65, 91)))
+    # Otherwise, render the single artist page
     artist = ARTISTS[id]
     releases = [RELEASES[x] for x in get_releases(artist)]
+    # Sort by ascending release date
+    # No problem with sorting in place since this list was created for this purpose
+    releases.sort(key=operator.itemgetter("reldate"))
     return render_template("single_artist.html", artist=artist, releases=releases)
 
 
